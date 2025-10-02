13170da630f214e0bcb7d95a208dbed0722874ce
damian mermod <damian.mermod@mac.com>
2025-10-02 13:11:33 +0200

    Added login, approve, register html site and styles.css

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('approve/', views.approve_view, name='approve'),
+]
