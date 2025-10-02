1f17ca4500a358ed56ab2c3208adcbbb12dfadca
damian mermod <damian.mermod@mac.com>
2025-10-02 17:00:49 +0200

    Added forms.py, I hope it works

---
+++ accounts/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+
+# ----------------------
+# Login-Form
+# ----------------------
+
+class UserLoginForm(UserCreationForm):
+    username = forms.CharField(
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control'})
+    )
+    password = forms.CharField(
+        required=True,
+        max_length=100,
+        widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control'})
+    )
+
+
+# ----------------------
+# Register-Form
+# ----------------------
+
+class UserRegistrationForm(UserCreationForm):
+    username = forms.CharField(
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control'})
+    )
+    email = forms.CharField(
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': 'form-control'})
+    )
+    password = forms.CharField(
+        required=True,
+        max_length=100,
+        widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control'})
+    )
+    confirm_password = forms.CharField(
+        required=True,
+        max_length=100,
+        widget=forms.PasswordInput(attrs={'placeholder': 'confirm Password', 'class': 'form-control'})
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'confirm_password']
