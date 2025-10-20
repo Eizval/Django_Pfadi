689c4c3ff9a64f72274515709937991480ac0c90
damian mermod <damian.mermod@mac.com>
2025-10-20 15:33:59 +0200

    Change a little of the styles and header scss

--- lager/views.py
+++ lager/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+@login_required
 def lager_edit_view(request):
     return render(request, "lager/lager_edit.html", {})
 
+@login_required
 def lager_list_view(request):
     return render(request, "lager/lager_list.html", {})
